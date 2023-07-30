8ad385c65874ed4b2e551bbaf9710690ed53a6dc
sunit sarkar <sunit6503@gmail.com>
2023-07-30 22:00:17 +0530

    update pending

--- router/blog.js
+++ router/blog.js
@@ -20,6 +20,51 @@ router.post('/create', (req, res) => {
     res.status(201).send('blog created')
 });
 
+router.get('/get', (req, res) => {
+    const email=req.query.email
+
+    let query = `SELECT id, email, blog FROM blogs WHERE email = ?`;
+    db.query(query, [email], (err, result) => {
+        if (err) {
+            console.log("error: ", err);
+            return res.status(500).json({
+                status: "Failed",
+                message: "Error occurred while fetching data from the database.",
+            });
+        }
+        res.status(200).json(result);
+    });
+});
+
+router.delete('/delete', (req, res) => {
+    const id=req.query.id;
+
+    let query = `DELETE FROM blogs WHERE id = ?`;
+    db.query(query, [id], (err, result) => {
+        if (err) {
+            console.log("error: ", err);
+            return res.status(500).json({
+                status: "Failed",
+                message: "Error occurred while deleting data from the database.",
+            });
+        }
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "No blog found with the provided email.",
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Blog deleted successfully.",
+        });
+    });
+});
+
+
+
 
 
 module.exports=router;
