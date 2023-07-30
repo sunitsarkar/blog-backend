a78ff3c3c60eedb5980c28e0baa6c28eae196dc7
sunit sarkar <sunit6503@gmail.com>
2023-07-30 23:17:28 +0530

    database password change

--- model/dbConfig.js
+++ model/dbConfig.js
@@ -3,7 +3,7 @@ const mysql=require('mysql')
 const db = mysql.createConnection({
     host: 'sql6.freemysqlhosting.net',
     user: 'sql6636326',
-    password: '	Please wait',
+    password: 'qE6yjwME7r',
     database: 'mynode'
 });
 
