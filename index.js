651a970c73408539329805d2f1c67d7b635108ab
sunit sarkar <124593285+sunitsarkar@users.noreply.github.com>
2023-07-31 12:48:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const blogRouter=require('./router/blog')
 const db=require('./model/dbConfig')
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://blog-app-sunit.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
