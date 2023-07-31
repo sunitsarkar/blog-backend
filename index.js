e89d0a0bff4617e9d406019634e805b4e4c3d6cf
sunit sarkar <sunit6503@gmail.com>
2023-07-31 11:56:01 +0530

    cors

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const userRouter=require('./router/user');
 const blogRouter=require('./router/blog')
 const db=require('./model/dbConfig')
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
+
   
 app.use(cors());
 app.use(bodyParser.json());
